bc9b5a9e7447a9b83c46ea5a6be957bc724f245c
Ariel Rozen <ariel.rozen@markets.com>
2017-03-19 11:11:58 +0200

    done

---
+++ src/api/baseUrl.js
@@ -0,0 +1,10 @@
+export default function getBaseUrl() {
+	return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+}
+
+function getQueryStringParameterByName(name) {
+	name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+	var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+		results = regex.exec(location.search);
+	return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
